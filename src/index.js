93903f87cc130c3d6350b2cbe61b4c57f2c9b4cd
kc-exe <75476926+kc-exe@users.noreply.github.com>
2022-03-20 12:26:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//var numbers = [3, 56, 2, 48, 5];
+
+////Map -Create a new array by doing something with each item in an array.
+//const newNumbers = numbers.map((x) => {
+//return x * 2;
+//});
+
+//////Filter - Create a new array by keeping the items that return true.
+//const newNumbers1 = numbers.filter((num) => {
+//return num < 10;
+//});
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+//var newNumber = numbers.reduce((accumulator, currentNumber) => {
+//return accumulator + currentNumber;
+//});
+
+////Find - find the first item that matches from an array.
+//const newNumber1 = numbers.find((num) => {
+//return num > 10;
+//});
+
+////FindIndex - find the index of the first item that matches.
+//const newNumber2 = numbers.findIndex((num) => {
+//return num > 10;
+//});
+
+//console.log(newNumbers, newNumbers1, newNumber, newNumber1, newNumber2);
